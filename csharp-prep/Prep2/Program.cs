94b9c616cdf7229fdf2e5369533a3d41fc1a0e87
Thomas Lovell <tdllovell@gmail.com>
2025-09-17 09:00:31 -0600

    Created the grade determiner, including sign

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade: ");
+        string gradeFromUser = Console.ReadLine();
+        int gradeValue = int.Parse(gradeFromUser);
+
+        string gradeLetter = "";
+        if (gradeValue >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (gradeValue >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradeValue >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradeValue >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        string gradeSign = "";
+        if (gradeValue < 95 && gradeValue >= 60)
+        {
+            int signDeterminer = gradeValue % 10;
+            if (signDeterminer >= 7)
+            {
+                gradeSign = "+";
+            }
+            else if (signDeterminer < 3)
+            {
+                gradeSign = "-";
+            }
+        }
+
+        Console.WriteLine($"Your grade is {gradeLetter}{gradeSign}.");
     }
 }
