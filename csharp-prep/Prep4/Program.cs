d4af2a5eb87cd636e3498f171f3653b9ee1d1d62
Thomas Lovell <tdllovell@gmail.com>
2025-09-24 09:10:58 -0600

    Created the numerical calculations program.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int userNumber;
+        List<int> numbers = new List<int>();
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        } while (userNumber != 0);
+
+        int sumNumbers = numbers.Sum();
+        double aveNumbers = numbers.Average();
+        int maxNumber = numbers.Max();
+
+        Console.WriteLine($"The sum of the numbers is {sumNumbers}.\nThe average of the numbers is {aveNumbers}.\nThe largest number is {maxNumber}.");
     }
 }
