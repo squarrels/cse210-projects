010273206dc14042febdd028961a0eb74ff4c4b1
Thomas Lovell <tdllovell@gmail.com>
2025-10-15 15:12:39 -0600

    Created the fraction program

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,40 @@
+﻿namespace Learning03;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop() { return _top; }
+    public int GetBottom() { return _bottom; }
+    public void SetTop(int top) {
+        _top = top;
+    }
+    public void SetBottom(int bottom) {
+        _bottom = bottom;
+    }
+    public string GetFractionString() {
+        string fraction = $"{_top}/{_bottom}";
+        return fraction;
+    }
+    public double GetDecimalValue() {
+        double fracValue = (double)_top / _bottom;
+        return fracValue;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using Learning03;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction blank = new Fraction();
+        Fraction whole = new Fraction(6);
+        Fraction part = new Fraction(6, 7);
+
+        Console.WriteLine(blank.GetDecimalValue());
+        Console.WriteLine(blank.GetFractionString());
+        Console.WriteLine(whole.GetFractionString());
+        Console.WriteLine(whole.GetDecimalValue());
+        Console.WriteLine(part.GetFractionString());
+        Console.WriteLine(part.GetDecimalValue());
     }
 }
